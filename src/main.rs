379e6826247b9e601e90c220d46c83cab398a847
123Brijesh44aa <oddsworld0121@gmail.com>
2024-10-24 20:52:50 +0530

    update : readme.md

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+use std::env;
+
+
+/*
+
+We can use the collect function to create many kinds of collections, so we explicitly annotate the type of args to specify that we want a vector of strings. Although you very rarely need to annotate types in Rust, collect is one function you do often need to annotate because Rust isn’t able to infer the kind of collection you want.
+
+*/
+
 fn main() {
-    
-    
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
 }
