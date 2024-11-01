b096b3413bfb3677d2f7150d351d6f2e2a04fefb
123Brijesh44aa <oddsworld0121@gmail.com>
2024-11-01 20:50:28 +0530

    update function new to build

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use core::panic;
 use std::env;
+use std::error;
 use std::fs;
+use std::os::windows::process;
+use std::process::exit;
 
 /*
 
@@ -11,7 +14,10 @@ We can use the collect function to create many kinds of collections, so we expli
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        exit(1);
+    });
 
     println!("Searching for '{}' ", config.query);
     println!("In file {}", config.file_path);
@@ -27,13 +33,13 @@ struct Config {
 }
  
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
     
-        Config{query,file_path}
+        Ok(Config{query,file_path})
     }
 }
