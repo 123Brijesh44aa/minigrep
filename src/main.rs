224e504092ca6cfb8a8edb2ad9c014d308afd4cc
123Brijesh44aa <oddsworld0121@gmail.com>
2024-11-01 21:14:49 +0530

    completed chapter 12.3  -> Refactoring to Improve Modularity and Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use core::panic;
 use std::env;
-use std::error;
-use std::fs;
-use std::os::windows::process;
 use std::process::exit;
+use minigrep::{run, Config};
 
 /*
 
@@ -22,24 +19,8 @@ fn main() {
     println!("Searching for '{}' ", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
- 
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config{query,file_path})
+    if let Err(e) = run(config){
+        println!("Application Error : {e}");
+        exit(1);
     }
 }
